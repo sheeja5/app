61e1f3fe891e02178f711dc9d57d7a7bfee632d9
sheeja5 <sheeja.stc@gmail.com>
2024-12-22 22:09:46 +0530

    Update about_me.py

--- about_me.py
+++ about_me.py
@@ -1,9 +1,9 @@
 import streamlit as st
 st.title("Introduction")
-st.write("NyaySathi
+st.write("""NyaySathi
 A perfect Ai for legal assistance
 
 This AI aims to make use of its knowledge to ensure that all the people of India are feeling safe. A peaceful surrounding is created with “ZERO” number of conflict among the people of India. this AI platform can also be situational based like if a person commits some kind of Crime by mistake this AI could help that person by leading him/her to a rightful way to how to deal with it .
 I created NyaySathi because I believe that everyone deserves access to justice, regardless of their income or background. In India, many people are unable to afford legal representation, which can lead to unfair outcomes in court.
  NyaySathi is designed to help people understand their legal rights and options, and to navigate the complex legal system. I hope that NyaySathi will empower people to fight for their rights and achieve justice. It can also lessen the number of Cyber Crimes that increasing daily.
-")
+""")
