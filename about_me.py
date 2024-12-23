dd887a2c71f8420c6acd0627724162d0d79bb661
sheeja5 <sheeja.stc@gmail.com>
2024-12-23 22:57:17 +0530

    Update about_me.py

--- about_me.py
+++ about_me.py
@@ -38,3 +38,6 @@ st.write("""
 """)
 if st.button('Nyaysathi'):
  st.switch_page("nyaysathi.py") 
+st.write("Founder and CEO --- Abel.S.Thomas ")
+st.write("/n")
+st.write("Co-Founder and Designer --- DivyaPrakash")
