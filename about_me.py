f5a1d70517a3d25394509b7dc4d77696af596f1c
sheeja5 <sheeja.stc@gmail.com>
2024-12-23 19:25:41 +0530

    Update about_me.py

--- about_me.py
+++ about_me.py
@@ -8,10 +8,13 @@ with col1:
 with col2:
   st.image("Screenshot (2).png", width=230)
 st.markdown("## **Nyaysathi**")
-st.image("DALL·E 2024-12-23 18.56.53 - A highly realistic and emotional illustration showing poor people living on the streets of India, with a backdrop of crowded streets and humble shelte.webp", width=230)
-st.markdown("""NyaySathi: A perfect legal Assistant
+c1,c2 = st.columns(2, gap="small", vertical_alignment="center")
+with c1:
+  st.image("DALL·E 2024-12-23 18.56.53 - A highly realistic and emotional illustration showing poor people living on the streets of India, with a backdrop of crowded streets and humble shelte.webp", width=230)
+with c2:
+  st.markdown("""NyaySathi: A perfect legal Assistant
 
-**Imagine you’re in a situation where someone refuses to return your hard-earned money or you’re tricked by an online scammer. What would you do? Most people don’t know where to start. Legal help feels expensive and complicated, leaving many without a fair chance to fight for their rights.**
+  **Imagine you’re in a situation where someone refuses to return your hard-earned money or you’re tricked by an online scammer. What would you do? Most people don’t know where to start. Legal help feels expensive and complicated, leaving many without a fair chance to fight for their rights.**
 
-""")
+  """)
 st.link_button('Try Nyaysathi now','https://2xbjtqbfkb2zwbev7prccj.streamlit.app/Nyaysathi')
