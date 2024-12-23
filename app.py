cef50e63763a11cdf455a0285f411a7780a792ed
sheeja5 <sheeja.stc@gmail.com>
2024-12-23 22:38:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
-
-# --- PAGE SETUP ---
+seleted = option_menu(
+    menu_title = "Menu",
+    options = ["About","Nyaysathi"],
+    icons = ["info-circle","robot"],
+    orientation = "horizontal",
+)
+if selected == "About":
+    st.switch_page(about_me.py)
 about_page = st.Page(
     "about_me.py",
     title="About",
