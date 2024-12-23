398d21c4a5eea652cb9b10d60468318b1800bd75
sheeja5 <sheeja.stc@gmail.com>
2024-12-23 11:34:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from streamlit_option_menu import option_menu
 selection = option_menu(
     menu_title = "Main Menu",
     options = ["About","Nyaysathi"],
-    icons = ["info","balance"]
+    icons = ["info","balance"],
     orientation = "horizontal",
 )
 about_page = st.Page(
