66e30c09911c3224e57d7df7e737f6cb78cd8d72
sheeja5 <sheeja.stc@gmail.com>
2024-12-23 22:47:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,30 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-selected = option_menu(
-    menu_title = "Menu",
-    options = ["About","Nyaysathi"],
-    icons = ["info-circle","robot"],
-    orientation = "horizontal",
+about_page = st.Page(
+    "about_me.py",
+    title="About",
+    icon=":material/account_circle:",
+    default=True,
 )
-if selected == "About":
-    st.switch_page("about_me.py")
-if selected == "Nyaysathi":
-    st.switch_page("nyaysathi.py")
-#about_page = st.Page(
-#    "about_me.py",
-#    title="About",
-#    icon=":material/account_circle:",
-#    default=True,
-#)
-#project_1_page = st.Page(
-#    "nyaysathi.py",
-#    title="Nyaysathi",
-#    icon=":material/balance:",
-#    default=False,
-#)
+project_1_page = st.Page(
+    "nyaysathi.py",
+    title="Nyaysathi",
+    icon=":material/balance:",
+    default=False,
+)
 
 
 # --- NAVIGATION SETUP [WITHOUT SECTIONS] ---
 # pg = st.navigation(pages=[about_page, project_1_page, project_2_page])
 
 # --- NAVIGATION SETUP [WITH SECTIONS]---
-#pg = st.navigation(
- #   {
-  #      "Info": [about_page],
-   #     "Projects": [project_1_page],
-    #}
-#)
+pg = st.navigation(
+    {
+        "Info": [about_page],
+        "Projects": [project_1_page],
+    }
+)
 
 
 # --- SHARED ON ALL PAGES ---
