9161c2d1403ccb5b3bf318414f000e703c20151b
sheeja5 <sheeja.stc@gmail.com>
2024-12-23 22:39:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-seleted = option_menu(
+selected = option_menu(
     menu_title = "Menu",
     options = ["About","Nyaysathi"],
     icons = ["info-circle","robot"],
