f277592c7d54789863ff08b1523d04530474e607
sheeja5 <sheeja.stc@gmail.com>
2024-12-23 08:31:44 +0530

    Update Nyaysathi.py

--- Nyaysathi.py
+++ Nyaysathi.py
@@ -9,7 +9,7 @@ import google.generativeai as genai
 
 st.title("Nyaysathi")
 
-USER_AVATAR = "👤"
+USER_AVATAR = ":material/account_circle:"
 BOT_AVATAR = ":material/robot_2:"
 genai.configure(api_key="AIzaSyDNcp_JDchji_PfU6mKwjCz2SxGBj0a4Ow")
 # Ensure model_name is initialized in session state
