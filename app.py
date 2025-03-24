5e95fb8bc1a37d4f7ead8f8378f0a6a35c8591fa
sheeja5 <sheeja.stc@gmail.com>
2025-03-24 07:35:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,8 +27,7 @@ project_2_page = st.Page(
 pg = st.navigation(
     {
         "Info": [about_page],
-        "Projects": [project_1_page],
-        "Projects": [project_2_page],
+        "Projects": [project_1_page, project_2_page],
         
     }
 )
